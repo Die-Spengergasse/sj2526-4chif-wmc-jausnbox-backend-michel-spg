bf50aece00840ccbfdb08f8f225fe7178a9ba2f1
mip <michel@spengergasse.at>
2025-10-10 12:27:52 +0200

    ADD Prisma ORM

---
+++ prisma/seed.js
@@ -0,0 +1,40 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+async function main() {
+  await prisma.recipe.create({
+    data: {
+      title: "Spaghetti Bolognese",
+      description: "Ein klassisches italienisches Gericht.",
+      duration: 45,
+      instructions: "Zuerst die Spaghetti kochen …",
+      ingredients: {
+        create: [
+          { name: "Spaghetti", quantity: "200g" },
+          { name: "Hackfleisch", quantity: "300g" },
+          { name: "Tomatensauce", quantity: "400ml" },
+        ],
+      },
+    },
+  });
+  await prisma.recipe.create({
+    data: {
+      title: "Pancakes",
+      description: "Weiche und fluffige Pfannkuchen.",
+      duration: 20,
+      instructions: "Alle Zutaten verrühren …",
+      ingredients: {
+        create: [
+          { name: "Mehl", quantity: "200g" },
+          { name: "Milch", quantity: "300ml" },
+          { name: "Eier", quantity: "2 Stück" },
+          { name: "Zucker", quantity: "2 EL" },
+        ],
+      },
+    },
+  });
+}
+main()
+  .catch((e) => console.error(e))
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
--- src/server.js
+++ src/server.js
@@ -1,23 +1,34 @@
-const express = require("express");
+// const express = require("express");
+import express from "express";
+import { PrismaClient } from "@prisma/client";
 const app = express();
 const port = 3001;
 
+const prisma = new PrismaClient();
+
 // Temp Rezepte laden
-const recipes = require("../assets/temp-data.json");
+// const recipes = require("../assets/temp-data.json");
 
 // URL: http://localhost:3000/api/recipes
-app.get("/api/recipes", (req, res) => {
-  res.json(recipes);
+app.get("/api/recipes", async (req, res) => {
+  const data = await prisma.recipe.findMany({
+    include: { ingredients: true },
+  });
+  res.json(data);
 });
 
 // URL: http://localhost:3000/api/recipes/1
 // URL: http://localhost:3000/api/recipes/3
 // URL: http://localhost:3000/api/recipes/10
-app.get("/api/recipes/:id", (req, res) => {
-  const id = req.params.id;
+app.get("/api/recipes/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
   console.log(id);
 
-  const recipe = recipes.find((recipe) => recipe.id === id);
+  // const recipe = recipes.find((recipe) => recipe.id === id);
+  const recipe = await prisma.recipe.findUnique({
+    where: { id: id },
+    include: { ingredients: true },
+  });
 
   if (!recipe) res.status(404).json({ message: "Recipe not found!" });
 
